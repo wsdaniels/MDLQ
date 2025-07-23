a809dc027840d2ac28f99fc25afd63623caf4d9c
William Daniels <wdaniels@sentinel.local>
2025-07-23 16:38:48 -0400

    implemented no overlap check at the source-level

--- code/MAIN_1_MDLQ.R
+++ code/MAIN_1_MDLQ.R
@@ -153,70 +153,73 @@ big.out <- foreach(a = 1:num.intervals) %dopar% {
   # Determine which sources have downwind sensors (i.e., which sources have information)
   info.mask <- apply(X, 2, function(this.col) sum(this.col > 0.5) > 4)
   
+  # Save information / no information mask
   q.hat[!info.mask] <-
     q.hat.lower[!info.mask] <- q.hat.upper[!info.mask] <-
     pis[!info.mask] <- q.hat.median[!info.mask] <- "no info"
   
+  # Subset to just the sources with information
+  X <- matrix(X[, info.mask], nrow = length(y))
+  
   # If all zeros, set emission rate for sources with information to zero
   if (all(y == 0)){
+    
     q.hat[info.mask] <-
       q.hat.lower[info.mask] <- q.hat.upper[info.mask] <-
       pis[info.mask] <- q.hat.median[info.mask] <- 0
     
   } else {
     
-    # subset to just the sources with information
-    X <- matrix(X[, info.mask], nrow = length(y))
+    # Determine which sources have overlap between simulated enhancements and enhancements in observations
+    overlap.mask <- apply(X, 2, function(this.col) sum(this.col[y > 0] > 1e-3) > 4)
     
-    # Check for overlap between enhancements in simulations and observations
-    if( sum(X[y > 0, ] > 1e-3) < 4 ){
-      # if( all(X[y > 0, ] < 1e-3) ){
+    # Save overlap / no overlap mask
+    q.hat[info.mask][!overlap.mask] <-
+      q.hat.lower[info.mask][!overlap.mask] <- q.hat.upper[info.mask][!overlap.mask] <-
+      pis[info.mask][!overlap.mask] <- q.hat.median[info.mask][!overlap.mask] <- "no overlap"
+    
+    # Subset to just the sources with overlap
+    X <- matrix(X[, overlap.mask], nrow = length(y))
+    
+    
+    # q.hat[info.mask] <-
+    #   q.hat.lower[info.mask] <- q.hat.upper[info.mask] <-
+    #   pis[info.mask] <- q.hat.median[info.mask] <- "invert"
+    
+    # Run the MDLQ model
+    out <- tryCatch(
+      { out <- ss.regress(y=y, X=X,
+                          n.samples = 2000,
+                          n.burn.in = 250)
+      }, error = function(msg){
+        out <- "DNC"
+        return(out)
+      }
+    )
+    
+    # catch any errors
+    if (length(out) == 1){
       q.hat[info.mask] <-
         q.hat.lower[info.mask] <- q.hat.upper[info.mask] <-
-        pis[info.mask] <- q.hat.median[info.mask] <- "no overlap"
+        pis[info.mask] <- q.hat.median[info.mask] <- out
       
+      # Save output
     } else {
+      these.rates <- out[, colnames(out) %in% paste0("beta", 1:ncol(X))]
+      these.pis <- out[, colnames(out) %in% paste0("pi", 1:ncol(X))]
       
-      # q.hat[info.mask] <-
-      #   q.hat.lower[info.mask] <- q.hat.upper[info.mask] <-
-      #   pis[info.mask] <- q.hat.median[info.mask] <- "invert"
+      these.rates <- as.matrix(these.rates, ncol = ncol(X))
+      these.pis <- as.matrix(these.pis, ncol = ncol(X))
       
-      # Run the MDLQ model
-      out <- tryCatch(
-        { out <- ss.regress(y=y, X=X,
-                            n.samples = 2000,
-                            n.burn.in = 250)
-        }, error = function(msg){
-          out <- "DNC"
-          return(out)
-        }
-      )
+      q.hat[info.mask][overlap.mask] <- apply(these.rates, 2, mean) * 3.6
+      q.hat.lower[info.mask][overlap.mask] <- apply(these.rates, 2, function(X) quantile(X, probs = 0.025)) * 3.6
+      q.hat.upper[info.mask][overlap.mask] <- apply(these.rates, 2, function(X) quantile(X, probs = 0.975)) * 3.6
       
-      # catch any errors
-      if (length(out) == 1){
-        q.hat[info.mask] <-
-          q.hat.lower[info.mask] <- q.hat.upper[info.mask] <-
-          pis[info.mask] <- q.hat.median[info.mask] <- out
-        
-        # Save output
-      } else {
-        these.rates <- out[, colnames(out) %in% paste0("beta", 1:ncol(X))]
-        these.pis <- out[, colnames(out) %in% paste0("pi", 1:ncol(X))]
-        
-        these.rates <- as.matrix(these.rates, ncol = ncol(X))
-        these.pis <- as.matrix(these.pis, ncol = ncol(X))
-        
-        q.hat[info.mask] <- apply(these.rates, 2, mean) * 3.6
-        q.hat.lower[info.mask] <- apply(these.rates, 2, function(X) quantile(X, probs = 0.025)) * 3.6
-        q.hat.upper[info.mask] <- apply(these.rates, 2, function(X) quantile(X, probs = 0.975)) * 3.6
-        
-        pis[info.mask] <- apply(these.pis, 2, mean)
-        
-        q.hat.median[info.mask] <- apply(these.rates, 2, median) * 3.6
-        
-      } # End if to check for MCMC convergence
+      pis[info.mask][overlap.mask] <- apply(these.pis, 2, mean)
       
-    } # End if to check for overlap
+      q.hat.median[info.mask][overlap.mask] <- apply(these.rates, 2, median) * 3.6
+      
+    } # End if to check for MCMC convergence
     
   } # End if to check for enhancements in observations
   
